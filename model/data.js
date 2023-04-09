65e32fca3551ab459657096dd3e3f2a9f21c710c
nguyenkien12345 <nguyenkien11202000@gmail.com>
2023-04-09 11:42:40 +0700

    Upload Project

---
+++ model/data.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { connection } = require('../connect');
+
+const dataSchema = new mongoose.Schema({
+    temperature: {
+        type: Number,
+        required: [true, 'Temperature is required'],
+    },
+    humidity: {
+        type: Number,
+        required: [true, 'Humidity is required'],
+    },
+    dust: {
+        type: Number,
+    },
+    active: {
+        type: Boolean,
+        default: true
+    },
+    date: {
+        type: String,
+    },
+    time: {
+        type: String
+    }
+}, { timestamps: true });
+
+mongoose.SchemaTypes.String.set('trim', true);
+
+module.exports = connection.model('data', dataSchema);
